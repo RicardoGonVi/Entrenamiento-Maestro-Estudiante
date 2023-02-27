78c8dc9d51d941027b419a1b7c48c10cb3411c6f
RicardoGonVi <ricardogonzalez.11.99@gmail.com>
2023-02-26 18:52:11 -0600

    Automatización de pruebas
    Se realizaron cambios para automatizar las pruebas para demostrar el mejor punto fps vs % de diferencia de los videos

--- ceropixels_vs_t2frames.py
+++ ceropixels_vs_t2frames.py
@@ -1,69 +1,80 @@
 import cv2
 
 # Nombre del archivo de video
-filename = "D:/TRABAJOS RICARDO/Semestre_13/PFG/Repositorio/Mingle/downsample/30min_day1_cam1_20fps_960x540.mp4"
+filevideo = "D:/TRABAJOS RICARDO/Semestre_13/PFG/Repositorio/Mingle/downsample/30min_day1_cam1_20fps_960x540.mp4"
 
-# Cantidad de frames por guardar durante cada segundo de video
-desiredfps = 1
+# Genera un archivo con los % de diferencia entre dos framse de un video
+def ceropixels_vs_t2frames(desiredfps, thvalue):
 
-# Cargando el archivo
-cap = cv2.VideoCapture(filename)
+    # Cargando el archivo
+    cap = cv2.VideoCapture(filevideo)
 
-# Obtiene el número de frames por segundo del video
-fps = cap.get(cv2.CAP_PROP_FPS)
+    # Obtiene el número de frames por segundo del video
+    fps = cap.get(cv2.CAP_PROP_FPS)
 
-# Obteniendo el primer frame
-ret, frame1 = cap.read()
+    # Obteniendo el primer frame
+    ret, frame1 = cap.read()
 
-# Contador para darle nombre a las imágenes
-count = 0
+    # Contador para saber cuantos frames se han procesado
+    count = 0
 
-# Loop del video
-while True:
-    # Obteniendo el siguiente frame
-    ret, frame2 = cap.read()
-    
-    # Termina loop cuando se recorren todos los frames
-    if not ret:
-        break
-    
-    # Si ha pasado suficiente tiempo desde el último frame comparado
-    if count % (fps/desiredfps) == 0 and count != 0:
+    # Archivo donde se guardan los resultados
+    fileresults = "Diff2frames/Prueba" + str(fps/desiredfps) + "fps" + str(thvalue) + "th.csv"
 
-        # Convirtiendo las imgs a escala de grises
-        gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-        gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    # Loop del video
+    while True:
+        # Obteniendo el siguiente frame
+        ret, frame2 = cap.read()
+        
+        # Termina loop cuando se recorren todos los frames
+        if not ret:
+            break
+        
+        # Calcula la diferencia entre 2 frames cada x frames / s
+        if count % (desiredfps) == 0 and count != 0:
 
-        # Calculando la diferencia entre frames
-        diff = cv2.absdiff(gray1, gray2)
+            # Convirtiendo las imgs a escala de gris
+            gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+            gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
-        # Binarizando la imagen
-        _, diff = cv2.threshold(diff, 255*0.1, 255, cv2.THRESH_BINARY)
+            # Calculando la diferencia entre frames
+            diff = cv2.absdiff(gray1, gray2)
 
-        # Mostrando la imagen de diferencia
-        #cv2.imshow('Difference', diff)
+            # Binarizando la imagen
+            _, diff = cv2.threshold(diff, thvalue, 255, cv2.THRESH_BINARY)
 
-        # Wait for a key press
-        #if cv2.waitKey(1) == ord('q'):
-        #    break
+            # Mostrando la imagen de diferencia
+            #cv2.imshow('Difference', diff)
 
-        # Contador de pixeles iguales
-        pixeles_negros = gray1.size - cv2.countNonZero(diff)
-        percentage = float(pixeles_negros/gray1.size*100)
+            # Wait for a key press
+            #if cv2.waitKey(1) == ord('q'):
+            #    break
 
-        # Save the number of zero pixels to a CSV file
-        with open('pixeles_negros.csv', 'a') as f:
-            f.write(str(percentage) + '\n')
+            # Contador de pixeles iguales
+            pixeles_negros = gray1.size - cv2.countNonZero(diff)
+            percentage = float(pixeles_negros/gray1.size*100)
 
-        # Set frame2 as the new frame1
-        frame1 = frame2
+            # Save the number of zero pixels to a CSV file
+            with open(fileresults, 'a') as f:
+                f.write(str(percentage) + '\n')
 
-    # Incrementa el contador
-    count += 1
+            # Set frame2 as the new frame1
+            frame1 = frame2
+
+        # Incrementa el contador
+        count += 1
+
+    # Release the video capture and close the window
+    cap.release()
+    print("Píxeles x frame: ", gray1.size)
+    cv2.destroyAllWindows()
 
 
+#desiredfps_list = [20, 10, 5, 1]
+thvalue_list = [0, 1, 25, 127]
+desiredfps_list = [40, 80, 160, 320]
+combination_list = []
 
-# Release the video capture and close the window
-cap.release()
-print("Píxeles x frame: ", gray1.size)
-cv2.destroyAllWindows()
+for nFPS in desiredfps_list:
+    for nTH in thvalue_list:
+        ceropixels_vs_t2frames(nFPS, nTH)
